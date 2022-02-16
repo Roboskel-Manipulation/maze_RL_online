d9631cf62a6c1d15905878a0c987a37f149c49dd
George Stavrinos <gstavrinos@protonmail.com>
2022-02-16 18:02:07 +0200

    Better installation process

--- install_dependencies/install.sh
+++ install_dependencies/install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
+sudo apt install python3-venv
 python3 -m venv env
 source env/bin/activate
 pip install -r install_dependencies/requirements.txt
-#export PYTHONPATH=$PWD:$PYTHONPATH
+export PYTHONPATH=$PWD:$PYTHONPATH
